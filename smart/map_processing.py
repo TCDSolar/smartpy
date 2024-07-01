18963997323038d3776f9ca0d8cd39f100e0c405
David Connolly <C21323196@mytudublin.ie>
2024-07-01 15:04:19 +0100

    cosine correction

--- smart/map_processing.py
+++ smart/map_processing.py
@@ -3,6 +3,8 @@ import skimage as ski
 from matplotlib import colors
 from skimage.morphology import disk
 
+import astropy.units as u
+
 from sunpy.map import Map, all_coordinates_from_map, coordinate_is_on_solar_disk
 
 
@@ -39,7 +41,7 @@ def STL(im_map):
     Parameters
     ----------
     im_map : sunpy.map.Map
-        Processed sunpy magnetogram map.
+        Processed SunPy magnetogram map.
 
     Returns
     -------
@@ -51,13 +53,17 @@ def STL(im_map):
         Boolean array indicating the sizes of each labeled region.
 
     """
+
+    cosmap = ar_cosmap(im_map)[0]
+    cosmap_data = cosine_correction(im_map, cosmap)[0]
+
     thresh = 100
-    negmask = im_map.data < -thresh
-    posmask = im_map.data > thresh
+    negmask = cosmap_data < -thresh
+    posmask = cosmap_data > thresh
     mask = negmask | posmask
 
     dilated_mask = ski.morphology.binary_dilation(mask, disk(5))
-    smoothed_data = ski.filters.gaussian(np.nan_to_num(im_map.data) * ~dilated_mask, sigma=16)
+    smoothed_data = ski.filters.gaussian(np.nan_to_num(cosmap_data) * ~dilated_mask, sigma=16)
     smooth_map = Map(smoothed_data, im_map.meta)
 
     labels = ski.measure.label(dilated_mask)
@@ -67,3 +73,76 @@ def STL(im_map):
     mask_sizes[0] = 0
     filtered_labels = mask_sizes[labels]
     return smooth_map, filtered_labels, mask_sizes
+
+
+def ar_cosmap(im_map):
+    """
+    Get the cosine map and off-limb pixel map using WCS.
+
+    Parameters
+    ----------
+    im_map : sunpy.map.Map
+        Processed SunPy magnetogram map.
+
+    Returns
+    -------
+    cos_cor : numpy.ndarray
+        Array of cosine correction factors for each pixel. Values greater than a threshold (edge) are set to 1.
+    d_angular : numpy.ndarray
+        Array of angular distances from the disk center in radians.
+    off_limb : numpy.ndarray
+        Binary array where pixels on the disk are 1 and pixels off the disk are 0.
+    """
+
+    ## Take off an extra percent from the disk to get rid of limb effects
+    edge = 0.99
+
+    x, y = np.meshgrid(*[np.arange(v.value) for v in im_map.dimensions]) * u.pixel
+    hp_coords = im_map.pixel_to_world(x, y)
+    xx = hp_coords.Tx.value
+    yy = hp_coords.Ty.value
+    d_radial = np.sqrt((xx**2.0) + (yy**2.0))
+
+    cos_cor = np.copy(d_radial)
+    cos_cor_ratio = cos_cor / im_map.meta["RSUN_OBS"]
+
+    cos_cor_ratio = np.clip(cos_cor_ratio, -1, 1)
+    d_angular = np.arcsin(cos_cor_ratio)
+    cos_cor = 1.0 / np.cos(d_angular)
+
+    off_disk = np.where(d_radial > (im_map.meta["RSUN_OBS"] * edge))
+    cos_cor[off_disk] = 1.0
+
+    off_limb = np.copy(d_radial)
+    off_disk_mask = np.where(d_radial >= (im_map.meta["RSUN_OBS"] * edge))
+    off_limb[off_disk_mask] = 0.0
+    on_disk_mask = np.where(d_radial < (im_map.meta["RSUN_OBS"] * edge))
+    off_limb[on_disk_mask] = 1.0
+
+    return cos_cor, d_angular, off_limb
+
+
+def cosine_correction(im_map, cosmap):
+    """
+    Perform magnetic field cosine correction.
+
+    Parameters
+    ----------
+    inmap : sunpy.map.Map
+        Processed SunPy magnetogram map.
+    cosmap : numpy.ndarray
+        An array of the cosine correction factors for each pixel.
+
+    Returns
+    -------
+    corrected_data : numpy.ndarray
+        The magnetic field data after applying the cosine correction.
+    cosmap : numpy.ndarray
+        The cosine correction factors, limited to the max value allowed.
+
+    """
+    angle_limit = np.arcsin(1.0 - im_map.meta["CDELT1"] / im_map.meta["RSUN_OBS"])
+    cos_limit = 1.0 / np.cos(angle_limit)
+    cosmap_limit = np.where((cosmap) > cos_limit)
+    cosmap[cosmap_limit] = cos_limit
+    return im_map.data * cosmap, cosmap
